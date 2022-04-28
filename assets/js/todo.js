69a73f55e74ba7a9cb7682de5fc0d813d30af16f
Paulo Lucena <paulo.henriquegvl@gmail.com>
2022-04-28 13:14:45 -0300

    all fix

---
+++ assets/js/todo.js
@@ -0,0 +1,12 @@
+const nome = "Paulo Lucena";
+
+let email = "paulolucena@email.com";
+
+console.log(`Nome: ${nome} Email: ${email}`);
+
+function escrever(x){
+    console.log("Frase: " + x);
+}
+
+escrever(`Seja bem vindo, ${nome}`);
+
